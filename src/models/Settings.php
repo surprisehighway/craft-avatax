55d93f51967672ec26b739ffa18b2afc258465be
Abry Rath <abry.rath@union.co>
2019-10-29 11:25:06 -0400

    Refactoring for autosuggest fields / env based config

--- src/models/Settings.php
+++ src/models/Settings.php
@@ -22,6 +22,13 @@ use craft\base\Model;
  */
 class Settings extends Model
 {
+    const ENV_DEBUG = '$AVATAX_DEBUG';
+    const ENV_TAX_CALC_ENABLED = '$AVATAX_TAX_CALC_ENABLED';
+    const ENV_COMMITTING_ENABLED = '$AVATAX_COMMITTING_ENABLED';
+    const ENV_ADDRESS_VALIDATION_ENABLED = '$AVATAX_ADDRESS_VALIDATION_ENABLED';
+    const ENV_PARTIAL_REFUNDS_ENABLED = '$AVATAX_PARTIAL_REFUNDS_ENABLED';
+    const ENV_ENVIRONMENT = '$AVATAX_ENV';
+
     // Public Properties
     // =========================================================================
 
@@ -254,5 +261,264 @@ class Settings extends Model
                 'boolean'
             ],
         ];
+    }
+
+    /**
+     * Get the value of accountId
+     *
+     * @return  string
+     */ 
+    public function getAccountId()
+    {
+        return Craft::parseEnv($this->accountId);
+    }
+
+    /**
+     * Get the value of licenseKey
+     *
+     * @return  string
+     */ 
+    public function getLicenseKey()
+    {
+        return Craft::parseEnv($this->licenseKey);
+    }
+
+    /**
+     * Get the value of companyCode
+     *
+     * @return  string
+     */ 
+    public function getCompanyCode()
+    {
+        return Craft::parseEnv($this->companyCode);
+    }
+
+    /**
+     * Get the value of sandboxAccountId
+     *
+     * @return  string
+     */ 
+    public function getSandboxAccountId()
+    {
+        return Craft::parseEnv($this->sandboxAccountId);
+    }
+
+    /**
+     * Get the value of sandboxLicenseKey
+     *
+     * @return  string
+     */ 
+    public function getSandboxLicenseKey()
+    {
+        return Craft::parseEnv($this->sandboxLicenseKey);
+    }
+
+    /**
+     * Get the value of sandboxCompanyCode
+     *
+     * @return  string
+     */ 
+    public function getSandboxCompanyCode()
+    {
+        return Craft::parseEnv($this->sandboxCompanyCode);
+    }
+
+    /**
+     * Get the value of shipFromName
+     *
+     * @return  string
+     */ 
+    public function getShipFromName()
+    {
+        return Craft::parseEnv($this->shipFromName);
+    }
+
+    /**
+     * Get the value of shipFromStreet1
+     *
+     * @return  string
+     */ 
+    public function getShipFromStreet1()
+    {
+        return Craft::parseEnv($this->shipFromStreet1);
+    }
+
+    /**
+     * Get the value of shipFromStreet2
+     *
+     * @return  string
+     */ 
+    public function getShipFromStreet2()
+    {
+        return Craft::parseEnv($this->shipFromStreet2);
+    }
+
+    /**
+     * Get the value of shipFromStreet3
+     *
+     * @return  string
+     */ 
+    public function getShipFromStreet3()
+    {
+        return Craft::parseEnv($this->shipFromStreet3);
+    }
+
+    /**
+     * Get the value of shipFromCity
+     *
+     * @return  string
+     */ 
+    public function getShipFromCity()
+    {
+        return Craft::parseEnv($this->shipFromCity);
+    }
+
+    /**
+     * Get the value of shipFromState
+     *
+     * @return  string
+     */ 
+    public function getShipFromState()
+    {
+        return Craft::parseEnv($this->shipFromState);
+    }
+
+    /**
+     * Get the value of shipFromZipCode
+     *
+     * @return  string
+     */ 
+    public function getShipFromZipCode()
+    {
+        return Craft::parseEnv($this->shipFromZipCode);
+    }
+
+    /**
+     * Get the value of shipFromCountry
+     *
+     * @return  string
+     */ 
+    public function getShipFromCountry()
+    {
+        return Craft::parseEnv($this->shipFromCountry);
+    }
+
+    /**
+     * Get the value of defaultTaxCode
+     *
+     * @return  string
+     */ 
+    public function getDefaultTaxCode()
+    {
+        return Craft::parseEnv($this->defaultTaxCode);
+    }
+
+    /**
+     * Get the value of defaultShippingCode
+     *
+     * @return  string
+     */ 
+    public function getDefaultShippingCode()
+    {
+        return Craft::parseEnv($this->defaultShippingCode);
+    }
+
+    /**
+     * Get the value of defaultDiscountCode
+     *
+     * @return  string
+     */ 
+    public function getDefaultDiscountCode()
+    {
+        return Craft::parseEnv($this->defaultDiscountCode);
+    }
+
+    /**
+     * Get the value of debug
+     *
+     * @return  boolean
+     */ 
+    public function getDebug()
+    {
+        $envValue = Craft::parseEnv(self::ENV_DEBUG);
+        if ($envValue !== self::ENV_DEBUG) {
+            return $envValue;
+        }
+        return $this->debug;
+    }
+
+    /**
+     * Get the value of enableTaxCalculation
+     *
+     * @return  boolean
+     */ 
+    public function getEnableTaxCalculation()
+    {
+        $envValue = Craft::parseEnv(self::ENV_TAX_CALC_ENABLED);
+        if ($envValue !== self::ENV_TAX_CALC_ENABLED) {
+            return $envValue;
+        }
+        return $this->enableTaxCalculation;
+    }
+
+    /**
+     * Get the value of enableCommitting
+     *
+     * @return  boolean
+     */ 
+    public function getEnableCommitting()
+    {
+        $envValue = Craft::parseEnv(self::ENV_COMMITTING_ENABLED);
+        if ($envValue !== self::ENV_COMMITTING_ENABLED) {
+            return $envValue;
+        }
+        return $this->enableCommitting;
+    }
+
+    /**
+     * Get the value of enableAddressValidation
+     *
+     * @return  boolean
+     */ 
+    public function getEnableAddressValidation()
+    {
+        $envValue = Craft::parseEnv(self::ENV_ADDRESS_VALIDATION_ENABLED);
+        if ($envValue !== self::ENV_ADDRESS_VALIDATION_ENABLED) {
+            return $envValue;
+        }
+        return $this->enableAddressValidation;
+    }
+
+    /**
+     * Get the value of enablePartialRefunds
+     *
+     * @return  boolean
+     */ 
+    public function getEnablePartialRefunds()
+    {
+        $envValue = Craft::parseEnv(self::ENV_PARTIAL_REFUNDS_ENABLED);
+        if ($envValue !== self::ENV_PARTIAL_REFUNDS_ENABLED) {
+            return $envValue;
+        }
+        return $this->enablePartialRefunds;
+    }
+
+    /**
+     * Get the value of environment
+     *
+     * @return  string
+     */ 
+    public function getEnvironment()
+    {
+        $envValue = Craft::parseEnv(self::ENV_ENVIRONMENT);
+        if ($envValue !== self::ENV_ENVIRONMENT) {
+            return $envValue;
+        }
+        return $this->environment;
+    }
+
+    public function isManuallySet($var)
+    {
+        return Craft::parseEnv($var) !== $var;
     }
 }
